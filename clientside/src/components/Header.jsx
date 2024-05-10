bec981d435ed5a6d1859142d142e1bbbe17ca227
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-05-10 14:20:25 +0300

    created credentials, solved cors problem , register and login forms and pages created

--- clientside/src/components/Header.jsx
+++ clientside/src/components/Header.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav class="bg-yellow-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <Link to={"/"}>Mern Auth</Link>
-        <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+    <nav className="bg-yellow-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link to="/">Mern Auth</Link>
+        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
               <Link
-                to={"/login"}
-                class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                to="/login"
+                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                 aria-current="page"
               >
                 Login
@@ -19,8 +19,8 @@ const Header = () => {
             </li>
             <li>
               <Link
-                to={"/register"}
-                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                to="/register"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Register
               </Link>
--- clientside/src/pages/Welcome.jsx
+++ clientside/src/pages/Welcome.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const [userInfo, setUserInfo] = useState();
+
+  const getUser = async () => {
+    try {
+      axios
+        .get("/api/auth/user")
+        .then((res) => setUserInfo(res.data))
+        .catch((err) => console.log("Error", err));
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+  return (
+    <div>
+      <h1>{userInfo ? userInfo.name : null}</h1>
+    </div>
+  );
 };
 
 export default Welcome;
